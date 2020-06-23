c6e7a37e7c72ee8934454561cce536636b986813
Tomasz Matras Work <tomasz.matras@developerweb.pl>
2020-06-23 13:12:34 +0200

    add api fn

--- script.js
+++ script.js
@@ -2,20 +2,34 @@ class App {
   constructor() {
     this.loadBtn = document.querySelector(".section__btn");
     this.loadBtn.addEventListener("click", this.loadCards);
+    this.divInfo = document.querySelector(".section__info");
+    this.h1 = document.querySelector(".section__title");
+    this.img = document.querySelector(".section__image");
+    this.p = document.querySelector(".section__additionalInfo");
     this.API = "https://api.pokemontcg.io/v1/cards";
+    this.pokemons =[]
   }
 
   takeData = () => {
     fetch(this.API)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data)=>this.pokemons.push(data))
+    
   };
+
   showData = () => {
-    console.log("OK");
+    const pokemons = [...this.pokemons]
+    const {id, name, hp, imageUrl} = pokemons[0].cards[0]
+    this.h1.textContent = `Nazwa: ${name}`;
+    this.img.src = `${imageUrl}`;
+    // this.p.textContent = `HP: ${hp}`
+    
+
   };
   loadCards = () => {
     this.takeData();
     this.showData();
+    
   };
 }
 const app = new App();
