c1c9ba88425bdef6ea2a88dec01fe62183426e1e
DawidWebb <tomasz.matras@developerweb.pl>
2020-06-24 23:34:49 +0200

    add api selector js for api

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const loadBtn = document.querySelector(".section__btn");
+const loadBtn = document.querySelector(".container__btn");
 
-const divInfo = document.querySelector(".section__info");
-const h1 = document.querySelector(".section__title");
-const img = document.querySelector(".section__image");
-const p = document.querySelector(".section__additionalInfo");
-const URL = "https://api.pokemontcg.io/v1/cards?page=1";
+const divSection = document.querySelector(".section");
+const URL = "https://api.pokemontcg.io/v1/cards?page=2&pageSize=4";
 let pokemons = null;
+const pokemonsName = [];
 
 fetch(URL)
   .then((res) => {
@@ -21,10 +19,21 @@ fetch(URL)
 
 showData = () => {
   pokemons.forEach((pokemon) => {
-    h1.textContent = `${pokemon.name}`;
+    const divInfo = document.createElement("div");
+    divInfo.classList.add("section__info");
+    const h3 = document.createElement("h3");
+    divInfo.appendChild(h3);
+    h3.textContent = `${pokemon.name}`;
+    const img = document.createElement("img");
+    divInfo.appendChild(img);
     img.src = `${pokemon.imageUrl}`;
-    p.textContent = `Nr: ${pokemon.number} Hp:${pokemon.hp} Dostępność${pokemon.rarity}`;
+    const p = document.createElement("p");
+    divInfo.appendChild(p);
+    p.textContent = `${pokemon.rarity}`;
+    divSection.appendChild(divInfo);
+    pokemonsName.push(pokemon.name);
   });
+  console.log(pokemonsName);
 };
 loadCards = () => {
   showData();
